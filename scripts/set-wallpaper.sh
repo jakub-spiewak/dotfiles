1f9d507bc47461c1894fdc786714b36816d9ae7b
Jakub Spiewak <priv@jakubspiewak.com>
2022-02-01 02:30:50 +0100

    feat: set wallpaper

---
+++ scripts/set-wallpaper.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+mkdir -p ~/.wallpapers
+
+unsplash-wallpaper -w 3840 -h 2400 -q 'nature' -d ~/.wallpapers &> /dev/null 
+
+wallpaper_path=$(ls -t ~/.wallpapers/wallpaper* | head -n 1)
+
+# wal -t -i ${wallpaper_path} &> /dev/null 
+feh --bg-scale ~/.wallpapers
+
+rm -r ~/.wallpapers
