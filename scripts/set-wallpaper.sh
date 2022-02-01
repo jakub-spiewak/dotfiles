c20198edde09cf26089de5207ed273bbdd996173
Jakub Spiewak <priv@jakubspiewak.com>
2022-02-02 00:34:32 +0100

    feat: change set wallpaper query

--- scripts/set-wallpaper.sh
+++ scripts/set-wallpaper.sh
@@ -2,7 +2,7 @@
 
 mkdir -p ~/.wallpapers
 
-unsplash-wallpaper -w 3840 -h 2400 -q 'nature' -d ~/.wallpapers &> /dev/null 
+unsplash-wallpaper -w 3840 -h 2400 -q 'wallpapers' -d ~/.wallpapers &> /dev/null 
 
 wallpaper_path=$(ls -t ~/.wallpapers/wallpaper* | head -n 1)
 
