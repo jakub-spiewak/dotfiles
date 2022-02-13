37910e90b59e2796173c5beb5e1be3938c51bdf9
Jakub Spiewak <priv@jakubspiewak.com>
2022-02-13 13:06:17 +0100

    feat: resoulution fix

--- scripts/launch-polybar.sh
+++ scripts/launch-polybar.sh
@@ -3,4 +3,12 @@
 
 killall -q polybar
 
-polybar -c $HOME/.config/polybar/polybar.conf mybar
+if type "xrandr"; then
+	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+		MONITOR=$m polybar --reload -c $HOME/.config/polybar/polybar.conf top & 
+	done
+else
+	polybar --reload -c $HOME/.config/polybar/polybar.conf top & 
+fi
+
+
--- scripts/screens-fix.sh
+++ scripts/screens-fix.sh
@@ -3,19 +3,21 @@
 randroutput=$(xrandr --query | grep -w connected)
 
 
-if [[ $randroutput == *"DP1-1"* ]]; then
-	echo connected 
-	xrandr --output DP1-1 --mode 1920x1080 --scale 2x2
-fi
+# if [[ $randroutput == *"DP-1-1"* ]]; then
+# 	echo connected 
+# 	xrandr --output DP-1-1 --mode 1920x1080 --scale 2x2
+# fi
 
-if [[ $randroutput == *"DP1-2"* ]]; then
-	echo connected 
-	xrandr --output DP1-2 --mode 1920x1080 --scale 2x2
-fi
+# if [[ $randroutput == *"DP-1-2"* ]]; then
+# 	echo connected 
+# 	xrandr --output DP-1-2 --mode 1920x1080 --scale 2x2
+# fi
 
 
-if [[ $randroutput == *"DP1-2"* && $randroutput == *"DP1-1"* ]]; then
-	xrandr --output eDP1 --pos 1920x2160
-	xrandr --output DP1-2 --pos 0x0
-	xrandr --output DP1-1 --pos 3840x0
+if [[ $randroutput == *"DP-1-2"* && $randroutput == *"DP-1-1"* ]]; then
+	xrandr --output eDP-1 --mode 1920x1200 --pos 960x1080
+	xrandr --output DP-1-2 --pos 0x0
+	xrandr --output DP-1-1 --pos 1920x0
+else
+	xrandr --output eDP-1 --mode 3840x2400 --pos 0x0
 fi
